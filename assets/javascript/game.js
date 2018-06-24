c265a367daf30c4e731f2398bb7936607d74bb7c
Peter Jarrard <peter.a.jarrard@gmail.com>
2018-06-23 21:13:27 -0500

    getting started

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+
+
+
+
+// First batch of values to choose from
+var words = [
+    "Pac Man",
+    "Tron",
+    "Pong",
+    "Centipede", 
+    "Galaga"
+   ]
+
+// Select from bonusWords array once user has solved all values in words array
+var bonusWords = [
+    "Space Invaders",
+    "Donkey Kong",
+    "Asteroids",
+    "Dig Dug",
+    "Frogger"
+   ]
+
+// Select from doubleBonus array once user has solved all values in bonusWords array
+var doubleBonus = [
+    "Missile Command",
+    "Tempest",
+    "Lunar Lander",
+    "Street Fighter",
+    "Paper Boy"
+   ]
+// ================   Variables   =====================================================================
+
+var wins = 0
+var guessLeft = placeholder
+var guessed = placeholder
+
+
+
+
+
+   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
