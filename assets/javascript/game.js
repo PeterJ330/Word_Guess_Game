09a420ca4aaadb5b01261cb776524516fd3e60ca
Peter Jarrard <peter.a.jarrard@gmail.com>
2018-06-23 23:25:01 -0500

    wip

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@
 
 // First batch of values to choose from
 var words = [
-    "Pac Man",
+    "Frogger",
     "Tron",
     "Pong",
     "Centipede", 
@@ -17,7 +17,7 @@ var bonusWords = [
     "Donkey Kong",
     "Asteroids",
     "Dig Dug",
-    "Frogger"
+    "Pac Man"
    ]
 
 // Select from doubleBonus array once user has solved all values in bonusWords array
@@ -30,9 +30,17 @@ var doubleBonus = [
    ]
 // ================   variables   =====================================================================
 
-var wins = 0
-var guessLeft = placeholder
-var guessed = placeholder
+var level = "placeholder" ;
+var wins = 0 ;
+var i = 0 ;
+var guessed = "placeholder" ;
+
+
+
+//function wordLength() {
+    //words.findIndex() }
+
+
 
 
 
@@ -40,23 +48,34 @@ var guessed = placeholder
 
 document.onkeyup = function(event) {
     console.log(event);
-}
 
-   
+// Determines which key was pressed.
+    var userGuess = event.key;
+    
+    document.getElementById("guessed").innerHTML = userGuess;  
+
+// Randomly chooses a choice from the options array. This is the word the user is trying to guess. 
 var computerGuess = words[Math.floor(Math.random() * words.length)];
-
-
-
-
+// Calculates initial # of guesses by adding the string length of the value the computerGuess variable returns
+// to 50% of that string length and rounding up to nearest integer
+var guessLeft = Math.ceil(computerGuess.length + (computerGuess.length * 0.5));
 
 
 
 // ================   html   =============================================================================
 
 var html = 
-//"<h4> Level: "  </h4>"
+"<p> Level: " + level + "  </p>" +
 "<p> Wins: " + wins + " </p>" +
 "<p> Guesses Remaining: " + guessLeft + " </p>" +
+// "<p> Letters Already Guessed: <div id="guessed"></div>: " + guessed + "</p>"; 
+//"<p> #guessed: " + guessed + " </p>";
 "<p> Letters Already Guessed: " + guessed + " </p>";
+
+
+document.querySelector("#game").innerHTML = html;
+// document.querySelector("#guessed").innerHTML = html;
+
+};
 
 
